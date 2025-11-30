ac328913f0c5e5ee318d69004eb4dcf6978e6165
Tanatswa Charmaine Mhiribidi <tanatswa.mhiribidi@ashesi.edu.gh>
2025-11-30 13:15:44 +0000

    database connect file changed to use both ports 3306 and 3307

--- db_connect.php
+++ db_connect.php
@@ -7,6 +7,10 @@ $dbname = "ubuntu_db";
 $conn = new mysqli($servername, $username, $password, $dbname);
 
 if ($conn->connect_error) {
+    $conn = new mysqli("localhost:3307", $username, $password,$dbname);
+    die("Connection failed: " . $conn->connect_error);
+}
+if ($conn->connect_error){
     die("Connection failed: " . $conn->connect_error);
 }
 ?>
